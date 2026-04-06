e8ae7f04adf2dc5c44ed886b54599c39da14bbd2
AbdelrahmanElnagdy-iOS <abdelrahman.elnagdy.eng@gmail.com>
2026-04-06 12:03:50 +0200

    project creation

---
+++ MultiBank stock tracker/MultiBank stock tracker/MultiBank_stock_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiBank_stock_trackerApp.swift
+//  MultiBank stock tracker
+//
+//  Created by Abdelrahman Elnagdy on 06/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MultiBank_stock_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
